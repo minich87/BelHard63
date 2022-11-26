bdd87582da9b249085cf8b0a1c8ccea974640d61
Andry <minich0912@gmail.com>
2022-11-26 12:25:52 +0300

    Fix code. With round, but can't comlex numbers

--- HOMEWORK/Lesson_3/task_2.py
+++ HOMEWORK/Lesson_3/task_2.py
@@ -1,26 +1,28 @@
 # Пользователь вводит 3 числа, найти средне арифметическое с точностью 3.
 
+from statistics import mean
+
 num1 = 0
 while num1 == 0:
     try:
-        num1 = complex(input('Enter number_1: '))
+        num1 = float(input('Enter number_1: '))
     except ValueError:
         print('Error! Enter only number.')
 
 num2 = 0
 while num2 == 0:
     try:
-        num2 = complex(input('Enter number_2: '))
+        num2 = float(input('Enter number_2: '))
     except ValueError:
         print('Error! Enter only number.')
 
 num3 = 0
 while num3 == 0:
     try:
-        num3 = complex(input('Enter number_3: '))
+        num3 = float(input('Enter number_3: '))
     except ValueError:
         print('Error! Enter only number.')
 
-sum_nums = num1 + num2 + num3
+sum_nums = (num1, num2, num3)
 
-print('Arithmetical mean:', sum_nums / 3)
+print('Arithmetical mean:', round(mean(sum_nums), 3))
