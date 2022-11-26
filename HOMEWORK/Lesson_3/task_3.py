a49149b3e4f444eb739b7e29c2e07c33c3c37a84
Andry <minich0912@gmail.com>
2022-11-26 13:17:13 +0300

    Correct syntax

--- HOMEWORK/Lesson_3/task_3.py
+++ HOMEWORK/Lesson_3/task_3.py
@@ -6,12 +6,10 @@ age = input('Enter your age: ')
 city = input('Enter your city: ')
 
 # way_1
-print('Hello! My name is', name + '. I\'m', age, 'years old. I\m from', city + '.')
+print('Hello! My name is', name + '. I\'m', age, 'years old. I\'m from', city + '.')
 
 # way_2
-print('Hello! My name is {0}. I\'m {1} years old. I\m from {2}.'
-     .format(name, age, city))
+print('Hello! My name is {0}. I\'m {1} years old. I\'m from {2}.'.format(name, age, city))
 
 # way_2
-print('Hello! My name is {n}. I\'m {a} years old. I\m from {c}.'
-     .format(n=name, a=age, c=city))
+print('Hello! My name is {n}. I\'m {a} years old. I\'m from {c}.'.format(n=name, a=age, c=city))
