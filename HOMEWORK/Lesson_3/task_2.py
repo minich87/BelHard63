7728ddf352a4a4e7fc2aaedc0e93024b0903466f
Andry <minich0912@gmail.com>
2022-11-29 23:46:34 +0300

    add function 'mean'

--- HOMEWORK/Lesson_3/task_2.py
+++ HOMEWORK/Lesson_3/task_2.py
@@ -1,9 +1,11 @@
 # Пользователь вводит 3 числа, найти средне арифметическое с точностью 3.
 
+from statistics import mean
+
 num1 = float(input('Enter number_1: '))
 num2 = float(input('Enter number_2: '))
 num3 = float(input('Enter number_3: '))
 
-sum_nums = (num1 + num2 + num3) / 3
+sum_nums = (num1, num2, num3)
 
-print('Arithmetical mean:', round(sum_nums, 3))
+print('Arithmetical mean:', round(mean(sum_nums), 3))
