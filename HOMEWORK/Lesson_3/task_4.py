8519a606e2878cfc222938e77b2e2566e109569f
Andry <minich0912@gmail.com>
2022-11-30 01:36:54 +0300

    Revert "fix variable value"
    This reverts commit 02084508375085eefb7c00c3ece050afb992313d.

--- HOMEWORK/Lesson_3/task_4.py
+++ HOMEWORK/Lesson_3/task_4.py
@@ -1,21 +1,21 @@
 # Пользователь вводит 3 числа, подсчитать сколько из них положительных / отрицательных
 
-num1 = None
-while num1 == None:
+num1 = 0
+while num1 == 0:
     try:
         num1 = float(input('Enter number_1: '))
     except ValueError:
         print('Error! Enter only number.')
 
-num2 = None
-while num2 == None:
+num2 = 0
+while num2 == 0:
     try:
         num2 = float(input('Enter number_2: '))
     except ValueError:
         print('Error! Enter only number.')
 
-num3 = None
-while num3 == None:
+num3 = 0
+while num3 == 0:
     try:
         num3 = float(input('Enter number_3: '))
     except ValueError:
@@ -30,7 +30,6 @@ for i in nums:
         count1 += 1
     elif i < 0:
         count2 += 1
-
 print('Positive numbers:', count1)
 print('Negative numbers:', count2)
 
