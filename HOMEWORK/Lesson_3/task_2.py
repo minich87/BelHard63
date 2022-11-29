b83f4e7250678fe3f1123be7d518e725de00bac3
Andry <minich0912@gmail.com>
2022-11-30 00:55:09 +0300

    fix function

--- HOMEWORK/Lesson_3/task_2.py
+++ HOMEWORK/Lesson_3/task_2.py
@@ -2,9 +2,26 @@
 
 from statistics import mean
 
-num1 = float(input('Enter number_1: '))
-num2 = float(input('Enter number_2: '))
-num3 = float(input('Enter number_3: '))
+num1 = None
+while num1 == None:
+    try:
+        num1 = float(input('Enter number_1: '))
+    except ValueError:
+        print('Error! Enter only number.')
+
+num2 = None
+while num2 == None:
+    try:
+        num2 = float(input('Enter number_2: '))
+    except ValueError:
+        print('Error! Enter only number.')
+
+num3 = None
+while num3 == None:
+    try:
+        num3 = float(input('Enter number_3: '))
+    except ValueError:
+        print('Error! Enter only number.')
 
 sum_nums = (num1, num2, num3)
 
