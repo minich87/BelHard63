9aca66d830f11a86624297cb60a50a47a60eecbb
Andry <minich0912@gmail.com>
2022-11-30 14:29:02 +0300

    fix type

--- HOMEWORK/Lesson_3/task_4.py
+++ HOMEWORK/Lesson_3/task_4.py
@@ -11,8 +11,8 @@ num2_is_negative = num2 < 0
 num3_is_positive = num3 > 0
 num3_is_negative = num3 < 0
 
-positive_nums = int(num1_is_positive) + int(num2_is_positive) + int(num3_is_positive)
-negative_nums = int(num1_is_negative) + int(num2_is_negative) + int(num3_is_negative)
+positive_nums = num1_is_positive + num2_is_positive + num3_is_positive
+negative_nums = num1_is_negative + num2_is_negative + num3_is_negative
 
 print('Positive numbers:', positive_nums)
 print('Negative numbers:', negative_nums)
