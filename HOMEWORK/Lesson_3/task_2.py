0891cd82a36705827c6cfe15ad81c0337c851f7b
Andry <minich0912@gmail.com>
2022-11-30 13:06:11 +0300

    fix code in task_2

--- HOMEWORK/Lesson_3/task_2.py
+++ HOMEWORK/Lesson_3/task_2.py
@@ -1,28 +1,9 @@
 # Пользователь вводит 3 числа, найти средне арифметическое с точностью 3.
 
-from statistics import mean
+num1 = float(input('Enter number_1: '))
+num2 = float(input('Enter number_2: '))
+num3 = float(input('Enter number_3: '))
 
-num1 = None
-while num1 == None:
-    try:
-        num1 = float(input('Enter number_1: '))
-    except ValueError:
-        print('Error! Enter only number.')
+sum_nums = (num1 + num2 + num3) / 3
 
-num2 = None
-while num2 == None:
-    try:
-        num2 = float(input('Enter number_2: '))
-    except ValueError:
-        print('Error! Enter only number.')
-
-num3 = None
-while num3 == None:
-    try:
-        num3 = float(input('Enter number_3: '))
-    except ValueError:
-        print('Error! Enter only number.')
-
-sum_nums = (num1, num2, num3)
-
-print('Arithmetical mean:', round(mean(sum_nums), 3))
+print('Arithmetical mean:', round(sum_nums, 3))
