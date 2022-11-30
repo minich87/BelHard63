d6ea199f4c7cda7544cbcd6cde6796418507b655
Andry <minich0912@gmail.com>
2022-11-30 14:23:41 +0300

    fix code in task_4

--- HOMEWORK/Lesson_3/task_4.py
+++ HOMEWORK/Lesson_3/task_4.py
@@ -4,10 +4,15 @@ num1 = float(input('Enter number_1: '))
 num2 = float(input('Enter number_2: '))
 num3 = float(input('Enter number_3: '))
 
-nums = str(num1) + str(num2) + str(num3)
+num1_is_positive = num1 > 0
+num1_is_negative = num1 < 0
+num2_is_positive = num2 > 0
+num2_is_negative = num2 < 0
+num3_is_positive = num3 > 0
+num3_is_negative = num3 < 0
 
-neg_nums = nums.count('-')
-pos_nums = 3 - neg_nums
+positive_nums = int(num1_is_positive) + int(num2_is_positive) + int(num3_is_positive)
+negative_nums = int(num1_is_negative) + int(num2_is_negative) + int(num3_is_negative)
 
-print('Positive numbers:', pos_nums)
-print('Negative numbers:', neg_nums)
+print('Positive numbers:', positive_nums)
+print('Negative numbers:', negative_nums)
