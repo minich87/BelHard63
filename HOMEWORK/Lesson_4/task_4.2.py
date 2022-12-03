35a0b620ff63d6760e12ee12dc26c5d521dda6ce
Andry <minich0912@gmail.com>
2022-12-03 13:25:59 +0300

    add code?????

--- HOMEWORK/Lesson_4/task_4.2.py
+++ HOMEWORK/Lesson_4/task_4.2.py
@@ -3,4 +3,11 @@
 
 text = input('Enter text: ')
 
-data = [i:i for i in text]
+data = [i: i for i in text]
+data_key = data.keys()
+data_char = data.values()
+
+print(text)
+print(data)
+print(data_key)
+print(data_char)
