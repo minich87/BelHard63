15a208f429abc687ed2898998ec63ba783e51928
Andry <minich0912@gmail.com>
2022-12-03 16:23:35 +0300

    add code

--- HOMEWORK/Lesson_4/task_4.3.py
+++ HOMEWORK/Lesson_4/task_4.3.py
@@ -4,8 +4,18 @@
 
 number_users = int(input("Enter amount users: "))
 
-users = {}
-users.fromkeys(i:)
-users_key = [i for i in range(0, number_users)]
+dict_key = (i for i in range(0, number_users+1))
 
-print(users)
+name_dict2 = 'User_' + str(i for i in dict_key)
+
+users = {}
+users.fromkeys(dict_key, name_dict2)
+
+user_key1 = 'name'
+user_key2 = 'email'
+
+
+
+
+print(dict_key)
+print(name_dict2)
