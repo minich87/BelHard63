2b9041edc9e5de8f6341a63a71c4a0d44b24f019
Andry <minich0912@gmail.com>
2022-12-04 01:08:42 +0300

    add code!

--- HOMEWORK/Lesson_4/task_4.3.py
+++ HOMEWORK/Lesson_4/task_4.3.py
@@ -4,16 +4,14 @@
 
 max_users = int(input('Enter amount users: '))
 
-numbers_users = [i for i in range(0, max_users + 1)]
-
 user = {'name': None, 'email': None}
 
-users = dict.fromkeys(numbers_users, user)
+users = {i: user for i in range(0, max_users + 1)}
 
-i = 0
-while i <= max_users:
-    users[i]['name'] = input(f'Enter user №{i} name: ')
-    users[i]['email'] = input(f'Enter user №{i} email: ')
-    i += 1
+#i = 0
+#while i <= max_users:
+    #users[i]['name'] = input(f'Enter user №{i} name: ')
+    #users[i]['email'] = input(f'Enter user №{i} email: ')
+    #i += 1
 
 print(users)
