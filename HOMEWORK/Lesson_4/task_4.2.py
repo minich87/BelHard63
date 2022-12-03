09a57e96edbaae8fa87eb725f8832358e6267dce
Andry <minich0912@gmail.com>
2022-12-03 13:39:14 +0300

    del code

--- HOMEWORK/Lesson_4/task_4.2.py
+++ HOMEWORK/Lesson_4/task_4.2.py
@@ -3,11 +3,10 @@
 
 text = input('Enter text: ')
 
-data = [i: i for i in text]
+data = {i: i for i in text}
 data_key = data.keys()
 data_char = data.values()
 
 print(text)
 print(data)
-print(data_key)
-print(data_char)
+
