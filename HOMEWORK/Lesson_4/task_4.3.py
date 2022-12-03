8d64d07d15f976718f20382fd6c44f38e5b82a13
Andry <minich0912@gmail.com>
2022-12-03 15:26:02 +0300

    add code

--- HOMEWORK/Lesson_4/task_4.3.py
+++ HOMEWORK/Lesson_4/task_4.3.py
@@ -1,3 +1,11 @@
 # Заполнить словарь где ключами будут выступать числа от 0 до n,
 # а значениями вложенный словарь с ключами "name", "email",
 # а их значения будут введены пользователем
+
+number_users = int(input("Enter amount users: "))
+
+users = {}
+users.fromkeys(i:)
+users_key = [i for i in range(0, number_users)]
+
+print(users)
