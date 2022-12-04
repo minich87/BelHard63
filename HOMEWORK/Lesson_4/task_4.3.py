ef0de381385b7c7d81930e1999a1c42d28d23e7b
Andry <minich0912@gmail.com>
2022-12-04 16:04:32 +0300

    fix code!

--- HOMEWORK/Lesson_4/task_4.3.py
+++ HOMEWORK/Lesson_4/task_4.3.py
@@ -4,14 +4,8 @@
 
 max_users = int(input('Enter amount users: '))
 
-user = {'name': None, 'email': None}
-
-users = {i: user for i in range(0, max_users + 1)}
-
-#i = 0
-#while i <= max_users:
-    #users[i]['name'] = input(f'Enter user №{i} name: ')
-    #users[i]['email'] = input(f'Enter user №{i} email: ')
-    #i += 1
+users = {i: {'name': input(f'Enter name User_{i}: '),
+             'email': input(f'Enter email User_{i}: ')}
+         for i in range(0, max_users + 1)}
 
 print(users)
