da0cce3dea6e9f719f87e7e71e8dcf70866f64f6
Andry <minich0912@gmail.com>
2022-12-07 12:21:21 +0300

    fix code!

--- HOMEWORK/Lesson_5/task_5.1.py
+++ HOMEWORK/Lesson_5/task_5.1.py
@@ -1,9 +1,30 @@
 # Вывести первые N чисел кратные M и больше K
 
-show_numbers = int(input('How many numbers to display? '))
-multiplicity = int(input('Enter multiplicity: '))
-min_number = int(input('Enter minimal value: '))
+show_numbers = None
+while show_numbers is None:
+    try:
+        show_numbers = int(input('How many numbers to display? '))
+    except ValueError:
+        print('Error! Need enter integer!')
 
-for i in range(0, 100000):
+multiplicity = None
+while multiplicity is None:
+    try:
+        multiplicity = float(input('Enter multiplicity: '))
+    except ValueError:
+        print('Error! Need enter number!')
+
+min_number = None
+while min_number is None:
+    try:
+        min_number = float(input('Enter minimal value: '))
+    except ValueError:
+        print('Error! Need enter number!')
+
+massif = list(range(-5**10, 5**10))
+result = []
+
+for i in massif:
     if i % multiplicity == 0 and i > min_number:
-        print(i(range(show_numbers)))
+        result.append(i)
+print(result[:show_numbers])
