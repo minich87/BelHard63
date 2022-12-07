9ea54cc4ee1b1b819c492d08e2987ebbaf435435
Andry <minich0912@gmail.com>
2022-12-07 14:11:56 +0300

    add code!

---
+++ HOMEWORK/Lesson_5/task_5.3.py
@@ -0,0 +1,14 @@
+# Вывести четные числа от 2 до N по 5 в строку.
+
+upper_limit = None
+while upper_limit is None:
+    try:
+        upper_limit = int(input('Enter upper limit: '))
+    except ValueError:
+        print('Please, enter integer!')
+
+massif = []
+for i in range(2, upper_limit+1):
+    if i % 2 == 0:
+        massif.append(i)
+print(massif)
