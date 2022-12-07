c4974cfe2560f169cc82d717e89cd65f8b7559f3
Andry <minich0912@gmail.com>
2022-12-07 16:30:08 +0300

    fix and fix code

--- HOMEWORK/Lesson_5/task_5.1.py
+++ HOMEWORK/Lesson_5/task_5.1.py
@@ -1,11 +1,14 @@
 # Вывести первые N чисел кратные M и больше K
 
 show_numbers = int(input('How many numbers to display? '))
-multiplicity = float(input('Enter multiplicity: '))
+multiplicity = int(input('Enter multiplicity: '))
 min_number = int(input('Enter minimal value: '))
 
-result = []
-for i in range(min_number+1):
+massiv = []
+for i in range(min_number+1, 10**10):
     if i % multiplicity == 0:
-        result.append(i)
-print(result[show_numbers:])
+        massiv.append(i)
+        if len(massiv) == show_numbers:
+            break
+
+print(massiv)
