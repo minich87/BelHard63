dd9083b12ef7fabb04b7b4bcc2fc0c7ff357bceb
Andry <minich0912@gmail.com>
2022-12-10 19:06:15 +0300

    fix code

--- HOMEWORK/Lesson_6/task_6.1.py
+++ HOMEWORK/Lesson_6/task_6.1.py
@@ -1,4 +1,10 @@
 # Написать функцию перевода десятичного числа в двоичное и обратно,
 # без использования функции int
 
-def conversion_in_bin():
+def bin_dec(number):
+    bin_num = bin(number)[2:]
+    dec_num = 0
+    for i in bin_num:
+        dec_num = dec_num * 2 + int(i)
+    print(bin_num)
+    print(dec_num)
