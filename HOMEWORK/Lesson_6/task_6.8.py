83d3582c2c14687f9847673b060d26f5878ada18
Andry <minich0912@gmail.com>
2022-12-11 22:17:42 +0300

    add code

--- HOMEWORK/Lesson_6/task_6.8.py
+++ HOMEWORK/Lesson_6/task_6.8.py
@@ -1,10 +1,17 @@
 # Словарь: ключ - название стараны, значения - список городов,
 # на вход поступает город, необходимо сказать из какой он страны.
 
+city = input('Enter city: ').title()
+
 country = {
-    'Belarus': ['Brest', 'Vitebsk', 'Gomel', 'Grodno', 'Minsk'],
-    'Germany': ['Berlin', 'Bonn', 'Cologne', 'Drezden', 'Munich'],
-    'France': ['Bordeuax', 'Nice', 'Lion', 'Marseilles', 'Paris'],
-    'Great Britain': ['Edinburgh', 'London', 'Liverpool', 'Manchester', 'Newcastle'],
-    'The USA': ['Boston', 'Chicago', 'Los Angeles', 'Miami', 'Washington'],
-    }
+        'Belarus': ['Brest', 'Vitebsk', 'Gomel', 'Grodno', 'Minsk'],
+        'Germany': ['Berlin', 'Bonn', 'Cologne', 'Drezden', 'Munich'],
+        'France': ['Bordeuax', 'Nice', 'Lion', 'Marseilles', 'Paris'],
+        'Great Britain': ['Edinburgh', 'London', 'Liverpool', 'Manchester', 'Newcastle'],
+        'The USA': ['Boston', 'Chicago', 'Los Angeles', 'Miami', 'Washington']
+        }
+
+for key, val in country.items():
+    for el in val:
+        if el == city:
+            print(key)
