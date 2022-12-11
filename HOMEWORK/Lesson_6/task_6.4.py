93d09b1255ac941d1e8772788611540711791b5d
Andry <minich0912@gmail.com>
2022-12-11 12:41:26 +0300

    I don't undestand?

--- HOMEWORK/Lesson_6/task_6.4.py
+++ HOMEWORK/Lesson_6/task_6.4.py
@@ -1,10 +1,15 @@
 # Список содержащий в себе различные типы данных, отфильтровать т.о.,
 # чтобы остались только строки, без использования доп. списка
 
-data = [566, 'Hello', 8.76, 'world', (67, 'Mersi!', 899, 9), '!',
-        ['hi', 365, 'earth', '!'], 'WELCOME', ',', 'to python']
+data = [0, 1, 2.2, 3, -4, 5, 6, True, 7, 8, 10, 5.66, 9, 'Hello', 8.76,
+        'world', [9, 'Mersi!', 99, False, 'Bingo!', 11], '!',
+        ['hi', 365, 'earth', 3, '!'], 'WELCOME', ',', 'to python']
 
-for i in data:
-    if i != str(i):
-        data.remove(i)
+for row in data:
+    if isinstance(row, (int, float, bool)):
+        data.remove(row)
+    elif isinstance(row, list):
+        for el in row:
+            if isinstance(el, (int, float, bool)):
+                row.remove(el)
 print(data)
