c4b5cf3fcd16183c603b791370abedca8f6f9aa2
Andry <minich0912@gmail.com>
2022-12-11 16:27:17 +0300

    add dict

---
+++ HOMEWORK/Lesson_6/task_6.8.py
@@ -0,0 +1,10 @@
+# Словарь: ключ - название стараны, значения - список городов,
+# на вход поступает город, необходимо сказать из какой он страны.
+
+country = {
+    'Belarus': ['Brest', 'Vitebsk', 'Gomel', 'Grodno', 'Minsk'],
+    'Germany': ['Berlin', 'Bonn', 'Cologne', 'Drezden', 'Munich'],
+    'France': ['Bordeuax', 'Nice', 'Lion', 'Marseilles', 'Paris'],
+    'Great Britain': ['Edinburgh', 'London', 'Liverpool', 'Manchester', 'Newcastle'],
+    'The USA': ['Boston', 'Chicago', 'Los Angeles', 'Miami', 'Washington'],
+    }
