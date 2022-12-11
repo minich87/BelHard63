ab511fb21583856ce0943f21bfdd2d3d3635ba14
Andry <minich0912@gmail.com>
2022-12-11 13:33:06 +0300

    fix data

--- HOMEWORK/Lesson_6/task_6.3.py
+++ HOMEWORK/Lesson_6/task_6.3.py
@@ -1,12 +1,9 @@
 # В списке чисел, необходимо сместить список на N-число
 
-dlina_list = int(input('Enter the length of the range of list: '))
 n_shift = int(input('Enter N-shift: '))
 
-data = []
-for i in range(1, dlina_list + 1):
-    data.append(i)
+data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-data = data[n_shift:] + data [:n_shift]
+data = data[n_shift:] + data[:n_shift]
 print(data)
 
