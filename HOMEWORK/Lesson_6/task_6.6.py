18f00501d360524ee42791a365dbd90c68aa638e
Andry <minich0912@gmail.com>
2022-12-14 00:54:30 +0300

    fix code

--- HOMEWORK/Lesson_6/task_6.6.py
+++ HOMEWORK/Lesson_6/task_6.6.py
@@ -1,17 +1,19 @@
 # Список рандомных чисел необходимо отсортировать его в виде,
 # сначала четные, потом нечетные
 
+def fist_even_numder(lst):
+    lst.sort()
+    lst_1 = []
+    lst_2 = []
+    for i in lst:
+        if i % 2:
+            lst_1.append(i)
+        else:
+            lst_2.append(i)
+        lst = lst_2 + lst_1
+    return lst
+
+
 data = [2, 5, 7, 34, 89, 93, 54, 32, 27, 276, 455, 132, 123, 3, 8, 53, 49, 76]
 
-data.sort()
-data_1 = []
-data_2 = []
-
-for i in data:
-    if i % 2:
-        data_1.append(i)
-    else:
-        data_2.append(i)
-data = data_2 + data_1
-
-print(data)
+print(fist_even_numder(data))
