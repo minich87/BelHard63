a1166c1ac5b326a1e5880d762935fcf4a7b693a0
Andry <minich0912@gmail.com>
2022-12-14 00:29:55 +0300

    fix code

--- HOMEWORK/Lesson_6/task_6.5.py
+++ HOMEWORK/Lesson_6/task_6.5.py
@@ -1,10 +1,12 @@
 # Список чисел необходимо развернуть без использования метода revese
 # и функции reversed, в так же доп. списка и среза
 
+def reverse_list(lst):
+    for i in range(int(len(lst) / 2)):
+        lst[i], lst[~i] = lst[~i], lst[i]
+    return lst
+
+
 data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-j = -1
-for i in range(int(len(data)/2)):
-    data[i], data[j] = data[j], data[i]
-    j -= 1
-print(data)
+print(reverse_list(data))
