45c742615bfcc5d8fe44c36ba4156e043a459a05
Andry <minich0912@gmail.com>
2022-12-13 23:33:19 +0300

    change code

--- HOMEWORK/Lesson_6/task_6.3.py
+++ HOMEWORK/Lesson_6/task_6.3.py
@@ -1,9 +1,16 @@
 # В списке чисел, необходимо сместить список на N-число
 
-n_shift = int(input('Enter N-shift: '))
+def offset_list(lst, offset):
+    if abs(offset) < len(lst):
+        return lst[-offset:] + lst[:-offset]
+    if abs(offset) == len(lst):
+        return lst
+    if abs(offset) > len(lst):
+        offset -= (offset // len(lst)) * len(lst)
+        return lst[-offset:] + lst[:-offset]
 
-data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-data = data[n_shift:] + data[:n_shift]
-print(data)
+print(offset_list([1, 2, 3, 4, 5, 6, 7], 3))
+print(offset_list([1, 2, 3, 4, 5, 6, 7], 7))
+print(offset_list([1, 2, 3, 4, 5, 6, 7], 11))
 
