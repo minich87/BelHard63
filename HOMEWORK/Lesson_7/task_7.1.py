641af46878aa9da3763204a3a9141785aee21ded
Andry <minich0912@gmail.com>
2022-12-16 12:59:38 +0300

    add code

--- HOMEWORK/Lesson_7/task_7.1.py
+++ HOMEWORK/Lesson_7/task_7.1.py
@@ -1,10 +1,10 @@
 # На вход функции подается целое число N, сгенерировать треугольник паскаля глубиной N
 
-def create_triangle(N):
+def triangle_Pascal(N):
     row = [1]
     for i in range(N):
         print(row)
         row = [sum(x) for x in zip([0] + row, row + [0])]
 
 
-create_triangle(10)
+triangle_Pascal(10)
