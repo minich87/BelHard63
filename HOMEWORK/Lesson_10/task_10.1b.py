9b33826a652335bbdbb0cfcc96b457599d0505da
Andry <minich0912@gmail.com>
2022-12-31 15:24:36 +0300

    add files with code for task 1

---
+++ HOMEWORK/Lesson_10/task_10.1b.py
@@ -0,0 +1,15 @@
+split_file = int(input('Enter a number to split file: '))
+split_string = int(input('Enter a number to split string: '))
+
+with open('file_with_few_strings.txt', 'r', encoding='utf-8') as file:
+    text = [line.strip() for line in file if line.strip()]
+
+text_add = [' '.join(text[split_string - 1:])]
+new_text = text[:split_string - 1] + text_add
+
+i = 0
+while i < split_file:
+    name_file = f'new_file_{1 + i}.txt'
+    with open(name_file, 'w', encoding='utf-8') as file:
+        file.write('\n'.join(new_text))
+        i += 1
