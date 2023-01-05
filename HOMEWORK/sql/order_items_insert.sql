f5b6e3d03d79985cfcd5ad0fc4f9a420409c45a9
Andry <minich0912@gmail.com>
2023-01-05 13:43:10 +0300

    add order_items

---
+++ HOMEWORK/sql/order_items_insert.sql
@@ -0,0 +1,33 @@
+import sqlite3
+
+
+conn = sqlite3.connect('db.sqlite3')
+cur = conn.cursor()
+
+
+cur.executemany('''
+INSERT INTO order_items(order_id, product_id) VALUES (?, ?);
+''', ((1, 1),
+      (1, 1),
+      (1, 2),
+      (1, 2),
+      (2, 3),
+      (3, 3),
+      (4, 3),
+      (1, 4),
+      (3, 5),
+      (4, 6),
+      (1, 7),
+      (2, 7),
+      (1, 8),
+      (3, 8),
+      (1, 9),
+      (3, 10),
+      (2, 11),
+      (4, 11),
+      (2, 12),
+      (1, 13),
+      (3, 13),
+      (4, 14),
+      (1, 15)))
+conn.commit()
