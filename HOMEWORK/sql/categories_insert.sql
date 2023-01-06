faa981cca84ede7f4d15c9618682ea4b7909acbb
Andry <minich0912@gmail.com>
2023-01-06 06:37:21 +0300

    fix %s

--- HOMEWORK/sql/categories_insert.sql
+++ HOMEWORK/sql/categories_insert.sql
@@ -4,7 +4,7 @@ conn = psycopg2.connect('postgresql://minich:12345@localhost:5432/bh63')
 with conn:
     with conn.cursor() as cur:
         cur.executemany('''
-            INSERT INTO categories(name) VALUES(?);
+            INSERT INTO categories(name) VALUES(%s);
                 ''', (('Laptop',), ('Smartphone',), ('Smartwatch',), ('Tablet',)))
         conn.commit()
 conn.close()
