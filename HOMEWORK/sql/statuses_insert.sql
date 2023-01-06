af1d3360a72c915aee431155925a8ad7ce3ed961
Andry <minich0912@gmail.com>
2023-01-06 06:50:42 +0300

    fix %s

--- HOMEWORK/sql/statuses_insert.sql
+++ HOMEWORK/sql/statuses_insert.sql
@@ -3,7 +3,7 @@ import psycopg2
 conn = psycopg2.connect('postgresql://minich:12345@localhost:5432/bh63')
 with conn:
     with conn.cursor() as cur:
-        cur.executemany('''INSERT INTO statuses(name) VALUES(?);
-        ''', (('active'',), ('deactive',), ('on_registration',)))
+        cur.executemany('''INSERT INTO statuses(name) VALUES(%s);
+            ''', (('active',), ('deactive',), ('registr.',)))
         conn.commit()
 conn.close()
--- HOMEWORK/sql/users_insert.sql
+++ HOMEWORK/sql/users_insert.sql
@@ -5,7 +5,7 @@ conn = psycopg2.connect('postgresql://minich:12345@localhost:5432/bh63')
 with conn:
     with conn.cursor() as cur:
         cur.executemany('''
-            INSERT INTO users(name, email) VALUES(?, ?);
+            INSERT INTO users(name, email) VALUES(%s, %s);
             ''', (('Alex', 'a.ivanov@mail.com'),
                   ('Ann', 'ann87@gmail.com'),
                   ('Nick', 'Nixs@tut.by'),
